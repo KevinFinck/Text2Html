c09e259f1c30223011ba38427195f623a987b7db
Kevin Finck <KevinFinck7@gmail.com>
2025-01-13 09:45:16 -0800

    Resizeable

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -1,59 +1,109 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using System.ComponentModel.DataAnnotations;
+using System.Diagnostics.Eventing.Reader;
+using TextCopy;
 
 namespace Text2Html.Pages
 {
-    public class IndexModel : PageModel
+    public class ParserModel : PageModel
     {
-        private readonly ILogger<IndexModel> _logger;
+        public string DefaultText { get; set; }
+            = @"
+        1.	Why is this Notice being issued?
+        2.	What is a class action and who is involved?
+        3.	Why is this lawsuit a class action?
+        4.	What is this lawsuit about?
+        5.	Has the Court decided who is right?
+        6.	What are Plaintiffs seeking?
+        7.	Is there money available now?
+        8.	Am I part of this Class?
+        9.	What happens if I do nothing at all?
+        10.	 How do I ask to be excluded from the Class?
+        11.	 Do I have a lawyer in this lawsuit?
+        12.	 Should I hire my own lawyer?
+        13.	 How will the lawyers be paid?
+        14.	 How and when will the Court decide who is right?
+        15.	 Do I have to attend the trial?
+        16.	 Will I get money after the trial?
+        17.	 Are more details available?
+        ";
 
-        [BindProperty] public TextParser Parser { get; set; }
+
+
+        private readonly ILogger<ParserModel> _logger;
 
         [BindProperty] 
+        public TextParser Parser { get; set; }
+
+        [BindProperty]
+        [DataType(DataType.MultilineText)]
+        [Display(Name = "Raw Input to Format:")]
         public string Input { get; set; }
 
         
         
         [BindProperty]
         [DataType(DataType.MultilineText)]
+        [Display(Name = "Parsed Output:")]
         public string ParsedOutput { get; set; }
 
 
+        //public ParserModel() { }
 
-        public IndexModel(ILogger<IndexModel> logger)
+        public ParserModel(ILogger<ParserModel> logger)
         {
             _logger = logger;
         }
 
         public IActionResult OnGet()
         {
+            Input = DefaultText;
             return Page();
         }
 
+        //public async Task<IActionResult> OnPostAsync()
 
-        public async Task<IActionResult> OnPostAsync()
+        //public IActionResult OnPostParse()
+        //{
+        //    //if (!ModelState.IsValid)
+        //    //{
+        //    //    ParsedOutput = "ModelState is invalid.";
+        //    //    return Page();
+        //    //}
+
+
+        //    if (string.IsNullOrWhiteSpace(ParsedOutput))
+        //        ParseModel();
+        //    else
+        //        CopyResultsToClipboard();
+
+        //    return Page();
+        //    //return RedirectToPage("./Index", this);
+        //}
+
+        public IActionResult OnPostParse()
         {
-            //if (!ModelState.IsValid)
-            //{
-            //    ParsedOutput = "ModelState is invalid.";
-            //    return Page();
-            //}
-
             var results = TextParser.ParseList(Input);
             if (string.IsNullOrWhiteSpace(results?.TheList) && string.IsNullOrWhiteSpace(results?.TextSection))
             {
                 ParsedOutput = "Nothing came back. :(";
+                ViewData["FormattedResult"] = ParsedOutput;
                 return Page();
             }
 
-            var toHtml = results.TheList.Replace(Environment.NewLine, "<br />");
+            var toHtml = results.TheList.Replace(Environment.NewLine, $"<br />{Environment.NewLine}");
+
             ParsedOutput = toHtml;
+            ViewData["FormattedResult"] = ParsedOutput;
 
             return Page();
-
-            //return RedirectToPage("./Index");
         }
 
+        public IActionResult OnPostCopyToClipboard(string textValue)
+        {
+            ClipboardService.SetText(textValue ?? "");
+            return Page();
+        }
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,34 +1,28 @@
 using Text2Html;
 using TextCopy;
 
-var parseMe = @"
-1.	Why was this Notice issued?
-2.	What is a class action?
-3.	What is this lawsuit about?
-4.	Why is there a Settlement?
-5.	How do I know if I am in the Settlement Class?
-6.	What does the settlement provide?
-7.	How much will my payment be?
-8.	When will I get my payment?
-9.	How do I get a payment?
-10.	What am I giving up if I stay in the Settlement Class?
-11.	Do I have a lawyer in this case?
-12.	How will the lawyers be paid?	
-13.	How do I get out of the Settlement?
-14.	If I don’t exclude myself, can I sue the Defendant for the same thing later?
-15.	If I exclude myself, can I get anything from this settlement?
-16.	How do I object to the Settlement?
-17.	What’s the difference between objecting and excluding myself from the Settlement?
-18.	When and where will the Court decide whether to approve the Settlement?
-19.	Do I have to come to the hearing?
-20.	May I speak at the hearing?
-21.	Where do I get more information??
-";
-
-
-var results = TextParser.ParseList(parseMe);
-ClipboardService.SetText(results?.TextSection ?? "");
-ClipboardService.SetText(results?.TheList ?? "");
+//var parseMe = @"
+//1.	Why is this Notice being issued?
+//2.	What is a class action and who is involved?
+//3.	Why is this lawsuit a class action?
+//4.	What is this lawsuit about?
+//5.	Has the Court decided who is right?
+//6.	What are Plaintiffs seeking?
+//7.	Is there money available now?
+//8.	Am I part of this Class?
+//9.	What happens if I do nothing at all?
+//10.	 How do I ask to be excluded from the Class?
+//11.	 Do I have a lawyer in this lawsuit?
+//12.	 Should I hire my own lawyer?
+//13.	 How will the lawyers be paid?
+//14.	 How and when will the Court decide who is right?
+//15.	 Do I have to attend the trial?
+//16.	 Will I get money after the trial?
+//17.	 Are more details available?
+//";
+//var results = TextParser.ParseList(parseMe);
+//ClipboardService.SetText(results?.TextSection ?? "");
+//ClipboardService.SetText(results?.TheList ?? "");
 
 
 
